1b0bf3caa16b5c44e2ce4ae72b041e31d8cb7f51
firminoneto11 <firminoneto11@hotmail.com>
2025-03-17 11:39:21 -0300

    chore: fixes deps

--- deps.sh
+++ deps.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo apt install xcb libxcb-xkb-dev x11-xkb-utils libx11-xcb-dev libxkbcommon-x11-dev libx11-dev libxtst-dev libpng-dev -y
+sudo apt-get install libx11-dev \
+    libx11-xcb-dev \
+    libxkbcommon-x11-dev \
+    libxtst-dev \
+    libpng-dev -y
